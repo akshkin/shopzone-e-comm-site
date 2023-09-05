f363c618fd4b8b2cb761c5f8536f111783126fef
akshkin <107682289+akshkin@users.noreply.github.com>
2023-09-05 14:41:17 +0200

    Change grid style on Category page

--- src/routes/categories/categories.tsx
+++ src/routes/categories/categories.tsx
@@ -1,57 +1,66 @@
 import React from "react";
-import { useParams, Outlet, LoaderFunctionArgs, defer, Await, useLoaderData } from "react-router-dom";
+import {
+  useParams,
+  Outlet,
+  LoaderFunctionArgs,
+  defer,
+  Await,
+  useLoaderData,
+} from "react-router-dom";
 import Product from "../../components/product/product.component";
 import { CategoryContainer } from "./categories.style";
-import { StyledLoader } from "../products/products.style";
+import { ProductsContainer, StyledLoader } from "../products/products.style";
 import { getProductsByCategory } from "../../utils/utils";
 import { ProductType } from "../../constants.types";
 
-
 type CategoryParams = {
   category: string;
-}
+};
 
-export function loader({ params }: LoaderFunctionArgs){
-  const productsPromise = params.category ? getProductsByCategory(params.category) : null
-  return defer({ categoryProducts: productsPromise })
+export function loader({ params }: LoaderFunctionArgs) {
+  const productsPromise = params.category
+    ? getProductsByCategory(params.category)
+    : null;
+  return defer({ categoryProducts: productsPromise });
 }
 
 type LoaderDataType = {
-  categoryProducts: CategoryProducts
-}
+  categoryProducts: CategoryProducts;
+};
 
 type CategoryProducts = {
-  products: ProductType[],
-    totalProducts: number
-}
-
+  products: ProductType[];
+  totalProducts: number;
+};
 
 function Category() {
   const { categoryProducts } = useLoaderData() as LoaderDataType;
   const { category } = useParams<keyof CategoryParams>() as CategoryParams;
 
-  function renderProducts(categoryProducts: CategoryProducts){
+  function renderProducts(categoryProducts: CategoryProducts) {
     return (
       <>
         <h4>Showing {categoryProducts.totalProducts} products</h4>
-        <CategoryContainer animate={{opacity: 1}} initial={{opacity: 0}} exit={{opacity: 0}}>
-          { categoryProducts.products.map((product) => {
-              return <Product key={product._id} product={product} />;
-            })}
-        </CategoryContainer>
+        <ProductsContainer
+          animate={{ opacity: 1 }}
+          initial={{ opacity: 0 }}
+          exit={{ opacity: 0 }}
+        >
+          {categoryProducts.products.map((product) => {
+            return <Product key={product._id} product={product} />;
+          })}
+        </ProductsContainer>
         <Outlet />
       </>
-
-    )
+    );
   }
 
-  
   return (
-    <>      
+    <>
       <h2>{category?.toUpperCase()}</h2>
       <React.Suspense fallback={<StyledLoader />}>
         <Await resolve={categoryProducts} children={renderProducts} />
-      </React.Suspense>      
+      </React.Suspense>
     </>
   );
 }
--- src/routes/searchPage/searchPage.tsx
+++ src/routes/searchPage/searchPage.tsx
@@ -1,71 +1,65 @@
 import { Suspense, useEffect } from "react";
-import { Await, defer, LoaderFunctionArgs, useLoaderData, useParams } from "react-router-dom";
+import {
+  Await,
+  defer,
+  LoaderFunctionArgs,
+  useLoaderData,
+  useParams,
+} from "react-router-dom";
 import Product from "../../components/product/product.component";
 import { ProductType } from "../../constants.types";
-import { productLoading, selectFilteredProducts } from "../../features/productsSlice";
+import {
+  productLoading,
+  selectFilteredProducts,
+} from "../../features/productsSlice";
 import { useAppDispatch, useAppSelector } from "../../hooks/useAppDispatch";
 import { getProductsBySearch } from "../../utils/utils";
 import { ProductsContainer, StyledLoader } from "../products/products.style";
 import { SearchTitle } from "./searchPage.styles";
 
-export function loader({ params }: LoaderFunctionArgs){
-  const dataPromise = params.query ? getProductsBySearch(params.query) : null
-  return defer({ filteredProducts: dataPromise })
+export function loader({ params }: LoaderFunctionArgs) {
+  const dataPromise = params.query ? getProductsBySearch(params.query) : null;
+  return defer({ filteredProducts: dataPromise });
 }
 
 type SearchParams = {
   query: string;
-}
+};
 
 type LoaderDataType = {
-  filteredProducts: ProductType[]
-}
+  filteredProducts: ProductType[];
+};
 
 function SearchPage() {
-  const { filteredProducts } = useLoaderData() as LoaderDataType
+  const { filteredProducts } = useLoaderData() as LoaderDataType;
   const { query } = useParams<keyof SearchParams>() as SearchParams;
-  const dispatch = useAppDispatch()
-  const loading = useAppSelector(productLoading)
-  //const filteredProducts = useAppSelector(selectFilteredProducts)
+  const dispatch = useAppDispatch();
+  const loading = useAppSelector(productLoading);
 
-  // useEffect(() => {
-  //   dispatch(searchProducts(query))
-
-  // }, [query, dispatch])
-  function renderFilteredProducts(filteredProducts: ProductType[]){
+  function renderFilteredProducts(filteredProducts: ProductType[]) {
     return (
-      <><SearchTitle>
-        Showing results for <span>{query}</span>
-      </SearchTitle>
-      <ProductsContainer>
-        {filteredProducts.length > 0 ? (
-          filteredProducts.map((product) => (
-            <Product key={product._id} product={product} />
-          ))
-        ) : (
-          <p>No results found. Please try another search!</p>
-        )}
-      </ProductsContainer></>
-    )
+      <>
+        <SearchTitle>
+          Showing results for <span>{query}</span>
+        </SearchTitle>
+        <ProductsContainer>
+          {filteredProducts.length > 0 ? (
+            filteredProducts.map((product) => (
+              <Product key={product._id} product={product} />
+            ))
+          ) : (
+            <p>No results found. Please try another search!</p>
+          )}
+        </ProductsContainer>
+      </>
+    );
   }
 
   return (
     <>
-    <Suspense fallback={<StyledLoader />}>
-      <Await resolve={filteredProducts} children={renderFilteredProducts} />
-    </Suspense>
-      {/* { loading? <StyledLoader /> :       (<><SearchTitle>
-        Showing results for <span>{query}</span>
-      </SearchTitle>
-      <ProductsContainer>
-        {filteredProducts.length > 0 ? (
-          filteredProducts.map((product) => (
-            <Product key={product._id} product={product} />
-          ))
-        ) : (
-          <p>No results found. Please try another search!</p>
-        )}
-      </ProductsContainer></>)} */}
+      <Suspense fallback={<StyledLoader />}>
+        <Await resolve={filteredProducts} children={renderFilteredProducts} />
+      </Suspense>
     </>
   );
 }
